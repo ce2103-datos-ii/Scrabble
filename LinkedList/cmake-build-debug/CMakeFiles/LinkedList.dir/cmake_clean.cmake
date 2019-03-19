ab519cc61165961627c0ad090a461e1b043e9a0b
JoseAgus00 <joseagustinveneveg@gmail.com>
2019-03-18 23:18:20 -0600

    LinkedList

---
+++ LinkedList/cmake-build-debug/CMakeFiles/LinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkedList.dir/main.cpp.o"
+  "CMakeFiles/LinkedList.dir/node.cpp.o"
+  "CMakeFiles/LinkedList.dir/list.cpp.o"
+  "LinkedList.pdb"
+  "LinkedList"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
